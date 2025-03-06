15bec3901854f2ded0be0e93748526c9b5c2d72e
Nikitha422 <127964247+Nikitha422@users.noreply.github.com>
2025-03-06 14:49:15 +0530

    Add files via upload

---
+++ characterNumber.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//// Function to get an integer from the user and return it as a character
+//char alp() {
+//    int x;
+//    printf("Enter the integer: ");
+//    scanf("%d", &x);
+//    return x;
+//}
+//
+//// Main function
+//int main() {
+//    printf("%c", alp()); // Call the alp function and print the result as a character
+//    return 0;
+//}
